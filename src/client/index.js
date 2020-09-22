446a98b36e8565a4ba40e1700d2a30a4e2b4419d
JoannaGil <joanna.gil09@gmail.com>
2020-09-22 12:48:32 -0400

    initial

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+import { requestAPI } from './js/evaluator'
+import { handleSubmit } from './js/formHandler'
+
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+export {
+  requestAPI,
+  handleSubmit,
+}
---
+++ src/server/index.js
@@ -0,0 +1,68 @@
+const dotenv = require('dotenv');
+dotenv.config();
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios')
+
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+const app = express()
+
+// You could call it aylienapi, or anything else
+// var aylien = require("aylien_textapi");
+// var textapi = new aylien({
+//     application_id: process.env.API_ID,
+//     application_key: process.env.API_KEY
+// });
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile('dist/index.html')
+})
+
+app.get("/algo", function (req, res) {
+    console.log(req)
+    res.json({ name: 'joanna', lastname: 'gil', ruta: req.query.url })
+})
+
+app.post("/algo", function (req, res) {
+    console.log(req)
+    res.json({ url: req.body.url })
+})
+
+app.post('/analyze', (req, res) => {
+
+    axios.post("https://api.meaningcloud.com/sentiment-2.1", {
+
+    },
+        {
+            params: {
+                key: "471f376ec6a72403cdec30019d9ac32d",
+                lang: "en",
+                txt: req.body.input
+            }
+        }).then(function (r) {
+
+            res.json(r.data);
+        }).catch(function (e) { console.log(e) })
+
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+
+
